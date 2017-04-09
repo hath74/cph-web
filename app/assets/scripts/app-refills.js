b464a8c915b410bba6a7715948416f3f250bad58
Daren Hathaway <hath74@yahoo.com>
2017-04-09 14:28:42 -0700

    Latest updates.

--- app/assets/scripts/app-refills.js
+++ app/assets/scripts/app-refills.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top - 120
+          scrollTop: target.offset().top - 190
         }, 1000);
         return false;
       }
